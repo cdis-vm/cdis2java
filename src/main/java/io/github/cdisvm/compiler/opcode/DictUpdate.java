b2b54459599ef26cd2df1bcb624fface8f3e8fc0
Christopher Chianelli <christopher@timefold.ai>
2026-08-08 21:09:49 -0400

    feat: implement dict update

--- src/main/java/io/github/cdisvm/compiler/opcode/DictUpdate.java
+++ src/main/java/io/github/cdisvm/compiler/opcode/DictUpdate.java
@@ -1,5 +1,13 @@
 package io.github.cdisvm.compiler.opcode;
 
+import java.lang.classfile.CodeBuilder;
+import java.util.Map;
+
+import io.github.cdisvm.compiler.CD;
+import io.github.cdisvm.compiler.CompilationRun;
+import io.github.cdisvm.compiler.MD;
+import io.github.cdisvm.compiler.StackMetadata;
+
 /**
  * Pop top of stack and merge it into the dict before it in the stack.
  * <p>
@@ -17,4 +25,11 @@ package io.github.cdisvm.compiler.opcode;
  * }</pre>
  */
 public record DictUpdate() implements Opcode {
+    @Override
+    public void implement(CodeBuilder codeBuilder, CompilationRun compilationRun, StackMetadata stackMetadata) {
+        codeBuilder.swap();
+        codeBuilder.dup_x1();
+        codeBuilder.swap();
+        codeBuilder.invokevirtual(CD.PY_DICT, "putAll", MD.of(void.class, Map.class));
+    }
 }
